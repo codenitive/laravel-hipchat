ce6ad725b9fd38098dc18f0416e20709838f2ca6
crynobone <crynobone@gmail.com>
2012-05-25 15:17:34 +0800

    Fixed #1 Load Hybrid if it's not registered

--- start.php
+++ start.php
@@ -4,3 +4,10 @@ Autoloader::map(array(
 	'Hipchat\\Room' => Bundle::path('hipchat').'classes/room'.EXT,
 	'Hipchat\\User' => Bundle::path('hipchat').'classes/user'.EXT,
 ));
+
+// Should load Hybrid bundle if it's not registered
+if ( ! Bundle::exists('hybrid'))
+{
+	Bundle::register('hybrid');
+	Bundle::start('hybrid');
+}
