98b112539b64cf77a2ad1c83f7de7d8b22341fb9
crynobone <crynobone@gmail.com>
2012-11-26 12:15:24 +0800

    Docblock update.

--- start.php
+++ start.php
@@ -1,10 +1,27 @@
 <?php
 
+/*
+|--------------------------------------------------------------------------
+| Hipchat Library
+|--------------------------------------------------------------------------
+|
+| Map Hipchat Library using PSR-0 standard namespace.
+|
+*/
+
 Autoloader::namespaces(array(
 	'Hipchat' => Bundle::path('hipchat').'libraries',
 ));
 
-// Should load Hybrid bundle if it's not registered
+/*
+|--------------------------------------------------------------------------
+| Load Bundle dependencies
+|--------------------------------------------------------------------------
+|
+| Should load Hybrid bundle if it's not registered.
+|
+*/
+
 if ( ! Bundle::exists('hybrid'))
 {
 	Bundle::register('hybrid');
